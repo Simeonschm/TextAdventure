c821fdaffb6d91a806c7dcc771d7566928da16f4
Redicker <danilo@redicker.de>
2018-10-07 14:36:38 +0200

    Methodensignaturen nach Implementationsdiagramm

--- Programm/src/Contoller/MainController.java
+++ Programm/src/Contoller/MainController.java
@@ -1,4 +1,19 @@
 package Contoller;
 
 public class MainController {
+
+    private Player player;
+
+    public MainController(){
+
+    }
+
+    private void createPlayer() {
+
+    }
+
+    public String getPlayerName() {
+
+    }
+
 }
