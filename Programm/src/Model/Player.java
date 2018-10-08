09dac5645fac8a58b65dd499b0617844d36badb8
sinemz <sinemz-01@outlook.com>
2018-10-08 10:36:47 +0200

    Erste Moduländerung

--- Programm/src/Model/Player.java
+++ Programm/src/Model/Player.java
@@ -1,4 +1,8 @@
 package Model;
 
 public class Player extends Figure {
+
+    public Player(String Name, int Strength, int Level){
+        super(Name, Strength, Level);
+    }
 }
--- Programm/src/Model/Princess.java
+++ Programm/src/Model/Princess.java
@@ -1,4 +1,8 @@
 package Model;
 
 public class Princess extends Figure {
+
+    public Princess(String Name, int Stength, int Level){
+        super(Name, Stength, Level);
+    }
 }
