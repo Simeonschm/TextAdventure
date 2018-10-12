9e3c2da0de00177673bd9ef9bd5845171f9cc09f
Simeonschm <simeonschmitt@icloud.com>
2018-10-12 08:04:48 +0200

    keine ahnung 2

--- Programm/src/View/GamePanelHandler1.java
+++ Programm/src/View/GamePanelHandler1.java
@@ -3,10 +3,26 @@ package View;
 import javax.swing.*;
 
 public class GamePanelHandler1 {
+
+
     private JButton button1;
     private JButton button2;
     private JButton button3;
     private JButton button4;
     private JTextField textField1;
     private JEditorPane editorPane1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+
+
+
+    public GamePanelHandler1() {
+
+    }
 }
